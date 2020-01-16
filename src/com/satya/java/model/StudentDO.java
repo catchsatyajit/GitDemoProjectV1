a48e4735a105e1eef4b11fa97bb9f296efe281a0
user <user@DESKTOP-U6HT6ET>
2020-01-16 12:42:34 +0530

    as suggested I have added required properties

--- src/com/satya/java/model/StudentDO.java
+++ src/com/satya/java/model/StudentDO.java
@@ -3,6 +3,10 @@ package com.satya.java.model;
 public class StudentDO {
 	private int id;
 	private String name;
+	private String address;
+	private String email;
+	private String mobile;
+	private String city;
 
 	public int getId() {
 		return id;
@@ -18,6 +22,38 @@ public class StudentDO {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 }
