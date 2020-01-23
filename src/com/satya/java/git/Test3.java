a8ca099dc2323de24fe24f6fbdfe4a1aeb396f46
user <user@DESKTOP-U6HT6ET>
2020-01-23 22:53:35 +0530

    commit test3

---
+++ src/com/satya/java/git/Test3.java
@@ -0,0 +1,15 @@
+package com.satya.java.git;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		String str="Yes";
+		if(str=="Yes" || str=="Y" || str=="y") {
+			System.out.println("Hi I am hungry.");
+		}else {
+			System.out.println("I am not hungry now.");
+		}
+
+	}
+
+}
